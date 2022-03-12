b272104723b2d3d5c0422955837345a0bbbd8d8a
enterprevic <38590322+enterprevic@users.noreply.github.com>
2022-03-12 03:55:18 +0300

    changes betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0
- * Return 0 if no issue
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
