342fd4cf24f9f6c9b1199b613775eb4a24facde9
enterprevic <38590322+enterprevic@users.noreply.github.com>
2022-03-12 03:56:41 +0300

    betty style fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
- * Return 0
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
