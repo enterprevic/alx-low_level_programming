7986f43e7a451802694176d4302eff72a34de314
Victor <enterprevic@gmail.com>
2022-03-16 04:32:41 +0300

    new changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,11 @@
  * main -  prints putchar, followed by a new line
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
 int main(void)
 {
     char text[8] = "_putchar";
@@ -10,10 +15,11 @@ int main(void)
 
     while (i < 8)
     {
-        puts(text[i]);
+        _putchar(text[i]);
         i++;
     }
-    puts('\n');
+    _putchar('\n');
 
     return 0;
 }
+
